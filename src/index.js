fcc3ea09d503451d0817e44de32ce0070c788e78
Tần <doantan1802@gmail.com>
2024-08-25 22:30:49 +0700

    add prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
-const express = require("express");
-const morgan = require("morgan");
-const { engine } = require("express-handlebars");
-const path = require("path");
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-const route = require("./routes");
+const route = require('./routes');
 
-app.use(
+    app.use(
     express.urlencoded({
         extended: true,
-    })
+    }),
 );
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP logger
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 // Template engine
 app.engine(
-    "hbs",
+    'hbs',
     engine({
-        extname: ".hbs",
-    })
+        extname: '.hbs',
+    }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Route init
-route(app)
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- src/routes/site.js
+++ src/routes/site.js
@@ -1,10 +1,10 @@
 // những trang không có tài nguyên cụ thể như home, search, contact
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const siteController = require("../app/controllers/SiteController");
+const siteController = require('../app/controllers/SiteController');
 
-router.use("/search", siteController.search);
-router.use("/", siteController.index);
+router.use('/search', siteController.search);
+router.use('/', siteController.index);
 
 module.exports = router;
