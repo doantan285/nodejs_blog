973f9d1942ea3c6de1badbb85cd5ce8679732d63
Tần <doantan1802@gmail.com>
2024-08-25 19:53:27 +0700

    restructure folder code

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, "public")));
 
 // HTTP logger
@@ -20,13 +29,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-    res.render("home");
-});
-
-app.get("/news", (req, res) => {
-    res.render("news");
-});
+// Route init
+route(app)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
